3a84863960d7db8ff68d9e7e666d27701b1057a2
Adi Rizka <26130158+adirizka7@users.noreply.github.com>
2021-07-09 06:46:05 +0700

    add simple avatar

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.white,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/26130158?v=4"),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.red,
-                  ),
-                ],
+              Text(
+                'Adi Rizka',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.grey,
+              Text(
+                'Software Engineer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
